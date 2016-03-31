ce9638ff4dedd5459fae81bc805d5a72d8be0c15
arnishow <arnishow@gmail.com>
2016-03-31 20:06:39 +0200

    .

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,6 @@ FROM alpine:3.3
 RUN apk add --update vsftpd
 RUN rm -rf /var/cache/apk/*
 
-RUN touch /var/log/vsftpd.log
-RUN chmod 600 /var/log/vsftpd.log
-
 COPY vsftpd.conf /etc/vsftpd/vsftpd.conf
 
 COPY entrypoint.sh /
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,4 @@ sed -i "s/^pasv_address=.*/pasv_address=$PASV_ADDRESS/" /etc/vsftpd/vsftpd.conf
 sed -i "s/^pasv_min_port=.*/pasv_min_port=$PASV_MIN_PORT/" /etc/vsftpd/vsftpd.conf
 sed -i "s/^pasv_max_port=.*/pasv_max_port=$PASV_MAX_PORT/" /etc/vsftpd/vsftpd.conf
 
-vsftpd /etc/vsftpd/vsftpd.conf
-exec tail -f /var/log/vsftpd.log
+exec vsftpd /etc/vsftpd/vsftpd.conf
