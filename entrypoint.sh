9c8a8c01d5fa1332958c0b4d013a4299372e0607
arnishow <arnishow@gmail.com>
2016-03-31 18:53:48 +0200

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sed -i "s/^pasv_address=.*/pasv_address=$PASV_ADDRESS/" /etc/vsftpd/vsftpd.conf
-sed -i "s/^pasv_min_port=.*/pasv_address=$PASV_MIN_PORT/" /etc/vsftpd/vsftpd.conf
-sed -i "s/^pasv_max_port=.*/pasv_address=$PASV_MAX_PORT/" /etc/vsftpd/vsftpd.conf
+sed -i "s/^pasv_min_port=.*/pasv_min_port=$PASV_MIN_PORT/" /etc/vsftpd/vsftpd.conf
+sed -i "s/^pasv_max_port=.*/pasv_max_port=$PASV_MAX_PORT/" /etc/vsftpd/vsftpd.conf
 
 exec vsftpd /etc/vsftpd/vsftpd.conf
