37d0e46f5c15433a4afaf2dbb11715f8a5c89485
arnishow <arnishow@gmail.com>
2016-04-05 19:09:45 +0200

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,9 @@ sed -i "s/^pasv_address=.*/pasv_address=$PASV_ADDRESS/" /etc/vsftpd/vsftpd.conf
 sed -i "s/^pasv_min_port=.*/pasv_min_port=$PASV_MIN_PORT/" /etc/vsftpd/vsftpd.conf
 sed -i "s/^pasv_max_port=.*/pasv_max_port=$PASV_MAX_PORT/" /etc/vsftpd/vsftpd.conf
 
+if [ -z "$USER" ] && [ -z "$PASSWD" ]; then
+  adduser -h /var/lib/ftp/ -D -H $USER
+  echo "seedbox:$PASSWD" | chpasswd
+fi
+
 exec vsftpd /etc/vsftpd/vsftpd.conf
