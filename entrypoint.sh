570b4b278d392732a5a456d409c1963c0aa074c9
arnishow <arnishow@gmail.com>
2016-04-06 12:44:11 +0200

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,12 @@ if [ -n "$USER" ] && [ -n "$PASSWD" ]; then
   echo "seedbox:$PASSWD" | chpasswd
 fi
 
+if [ -n "$SSL_DATA" ]; then
+  echo "ssl_enable=YES" >> /etc/vsftpd/vsftpd.conf
+  echo "force_local_data_ssl=$SSL_DATA" >> /etc/vsftpd/vsftpd.conf
+  echo "force_local_logins_ssl=YES" >> /etc/vsftpd/vsftpd.conf
+  echo "rsa_cert_file=/etc/vsftpd/$SSL_CERT" >> /etc/vsftpd/vsftpd.conf
+  echo "rsa_private_key_file=/etc/vsftpd/$SSL_KEY" >> /etc/vsftpd/vsftpd.conf
+fi
+
 exec vsftpd /etc/vsftpd/vsftpd.conf
