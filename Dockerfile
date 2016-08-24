beb141c4edeebd8ca32ad3db66bbb18ddfb9667c
Joel Rowley <hjoelr@gmail.com>
2016-08-24 13:48:04 -0400

    Major changes * Support Ctrl + C to kill container * Start of using passwd file * Tweaked vsftpd.conf

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,18 @@ FROM debian:jessie
 
 RUN export DEBIAN_FRONTEND='noninteractive' && \
     apt-get update -qq && \
-    apt-get install -qqy --no-install-recommends vsftpd && \
+    apt-get install -qqy --no-install-recommends vsftpd openssl && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /tmp/*
 
-RUN mkdir -p /etc/vsftpd /var/run/vsftpd/empty
+ENV LOG_FILE=/var/log/vsftpd.log \
+    SSL=false \
+    PAM_FILE=/etc/vsftpd/vsftpd.passwd
+
+RUN mkdir -p /etc/vsftpd /var/run/vsftpd/empty && \
+    echo "auth required pam_pwdfile.so pwdfile ${PAM_FILE}" > /etc/pam.d/vsftpd && \
+    echo "account required pam_permit.so" >> /etc/pam.d/vsftpd
+
 COPY vsftpd.conf /etc/vsftpd/vsftpd.conf
 
 COPY entrypoint.sh /
@@ -15,3 +22,4 @@ RUN chmod +x /entrypoint.sh
 EXPOSE 21/tcp
 
 ENTRYPOINT ["/entrypoint.sh"]
+CMD ["vsftpd", "/etc/vsftpd/vsftpd.conf"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,49 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 sed -i "s/^pasv_address=.*/pasv_address=$PASV_ADDRESS/" /etc/vsftpd/vsftpd.conf
 sed -i "s/^pasv_min_port=.*/pasv_min_port=$PASV_MIN_PORT/" /etc/vsftpd/vsftpd.conf
 sed -i "s/^pasv_max_port=.*/pasv_max_port=$PASV_MAX_PORT/" /etc/vsftpd/vsftpd.conf
 
 if [ -n "$USER" ] && [ -n "$PASSWD" ]; then
-  useradd -d /srv/ftp -M $USER
-  #echo "$USER:$PASSWD" | chpasswd -e
-  echo "$USER:$PASSWD" | chpasswd
+    printf "User: %s\nPass: %s\n" "$USER" "$PASSWD"
+    useradd -d /srv/ftp -M $USER
+    #echo "$USER:$PASSWD" | chpasswd -e
+    echo "$USER:$PASSWD" | chpasswd
+    echo "Changed password for '$USER' to '$PASSWD'."
 fi
 
 if [ $SSL = "YES" ]; then
-  echo "ssl_enable=YES" >> /etc/vsftpd/vsftpd.conf
-  echo "force_local_logins_ssl=YES" >> /etc/vsftpd/vsftpd.conf
-  if [ -n "$SSL_DATA" ]; then
-    echo "force_local_data_ssl=$SSL_DATA" >> /etc/vsftpd/vsftpd.conf
-  fi
-  echo "rsa_cert_file=/etc/vsftpd/vsftpd.cert.pem" >> /etc/vsftpd/vsftpd.conf
-  echo "rsa_private_key_file=/etc/vsftpd/vsftpd.key.pem" >> /etc/vsftpd/vsftpd.conf
+    echo "ssl_enable=YES" >> /etc/vsftpd/vsftpd.conf
+    echo "force_local_logins_ssl=YES" >> /etc/vsftpd/vsftpd.conf
+    if [ -n "$SSL_DATA" ]; then
+        echo "force_local_data_ssl=$SSL_DATA" >> /etc/vsftpd/vsftpd.conf
+    fi
+    echo "rsa_cert_file=/etc/vsftpd/vsftpd.cert.pem" >> /etc/vsftpd/vsftpd.conf
+    echo "rsa_private_key_file=/etc/vsftpd/vsftpd.key.pem" >> /etc/vsftpd/vsftpd.conf
 fi
 
-exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+#ln -sf /dev/stdout $LOG_FILE
+
+# Trap code borrowed from https://github.com/panubo/docker-vsftpd/blob/master/entry.sh
+function vsftpd_stop() {
+  echo "Received SIGINT or SIGTERM. Shutting down vsftpd"
+  # Get PID
+  pid=$(cat /var/run/vsftpd/vsftpd.pid)
+  # Set TERM
+  kill -SIGTERM "${pid}"
+  # Wait for exit
+  wait "${pid}"
+  # All done.
+  echo "Done"
+}
+
+if [ "$1" == "vsftpd" ]; then
+  trap vsftpd_stop SIGINT SIGTERM
+  echo "Running $@"
+  $@ &
+  pid="$!"
+  echo "${pid}" > /var/run/vsftpd/vsftpd.pid
+  wait "${pid}" && exit $?
+else
+  exec "$@"
+fi
